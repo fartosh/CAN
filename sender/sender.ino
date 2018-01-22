515e2fed00daea11cbbf8c3c445f7d84750abbd0
fartosh <bartosz.piasecki1994@gmail.com>
2018-01-22 15:41:59 +0100

    poprawki w senderze

--- sender/sender.ino
+++ sender/sender.ino
@@ -25,47 +25,53 @@ void setup()
     Serial.println("CAN BUS Shield init ok!");
 }
 
-
-
 char temperature[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-char humidity[8] = {0, 0, 0, 0, 0, 0, 1, 1};
-String temp_buf;
-String hum_buf;
+char humidity[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-
-String get_temp(void)
+int get_temp(void)
 {
   //TO DO: communication with temperature sensor
   temp = temp + random (-100,100);
-  return String(temp);
+  return temp;
 }
 
-String get_hum(void)
+int get_hum(void)
 {
   //TO DO: communication with temperature sensor
   hum = hum + random (-10,10);
-  return String(hum);
+  return hum;
+}
+
+void prepare_data(void)
+{
+    int temp_buf = get_temp();  //simulation of temperature sensor
+    int hum_buf = get_hum();  //simulation of humidity sensor
+    
+    //parsing data onto unsigned char array
+    temperature[7] = (temp_buf % 10);
+    temperature[6] = (temp_buf % 100) / 10;
+    temperature[5] = (temp_buf % 1000) / 100;
+    temperature[4] = (temp_buf / 1000);
+    
+    humidity[7] = (hum_buf % 10);
+    humidity[6] = (hum_buf % 100) / 10;
+    humidity[5] = (hum_buf / 100);
+
+//    Serial.println(temperature);
+//    Serial.println(sizeof(temperature));
+//    Serial.println(humidity);
+//    Serial.println(sizeof(humidity));     
 }
 
 void loop()
 {
     // get data from sensors
-    temp_buf = get_temp();
-    hum_buf = get_hum();
-    temp_buf.toCharArray(temperature, 8);
-    hum_buf.toCharArray(humidity, 8);
-    // humidity = 
-    // parse data
-    Serial.println(temperature);
-    Serial.println(sizeof(temperature));
-    
-    Serial.println(humidity);
-    Serial.println(sizeof(humidity));
-    
-    //We can either send a messange through a specified buffer or through a first found free buffer
-    //It may be neccessary to use specific buffer when there are different prorities to the informations of if we need to store old informations
-    //In this assignment there is no need to buffor data, as we do not need old data at any time
-    //Its better to look for the first free buffor and just go for it
+    prepare_data();
+  
+    //We can either send a messange through a specified buffer or through a first free buffer found.
+    //It may be neccessary to use specific buffer when there are different prorities to the informations of if we need to store old informations.
+    //In this assignment there is no need to buffor data, as we do not need old data at any time.
+    //Its better to look for the first free buffor and just go for it.
     
     // send data:  id = 0xXX, frame type (0 - standard frame), data len , data buf
     CAN.sendMsgBuf(0x02, 0, 8, (unsigned char *)temperature);
